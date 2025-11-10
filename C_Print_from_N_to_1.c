574b3242951c5aadc1325d25a1d0b42ca32230db
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-11-11 00:14:40 +0600

    practice 2

---
+++ C_Print_from_N_to_1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void print(int n, int i)
+{
+    if (i == n + 1)
+    {
+        return;
+    }
+    print(n, i + 1);
+    if (i == 1)
+    {
+        printf("%d", i);
+    }
+    else
+    {
+        printf("%d ", i);
+    }
+}
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    print(n, 1);
+    return 0;
+}
